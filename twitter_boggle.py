d0cfb97d63a80274dc67d1b10c2dd359c2627999
jonhartm <jonhartm@umich.edu>
2018-02-11 01:41:22 -0500

    pulled code into if __main__ and added most common shared words function

--- twitter_boggle.py
+++ twitter_boggle.py
@@ -21,15 +21,28 @@ auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
 stop_words = stopwords.words('english') # from https://pythonspot.com/nltk-stop-words/
 stop_words.extend(['http', 'https', 'RT'])
 
-freq_list_by_account = {}
-for account in twitter_accounts:
-    url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
-    params = {'screen_name':account, 'count':num_tweets}
-    responses = caching.Check(url, params, auth)
-    tokens = []
-    for response in responses:
-        for word in nltk.word_tokenize(response['text']):
-            word = word.lower()
-            if word.isalpha() and word not in stop_words:
-                tokens.append(word)
-    freq_list_by_account[account] = nltk.FreqDist(tokens)
+if __name__ == '__main__':
+    freq_list_by_account = {}
+    for account in twitter_accounts:
+        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        params = {'screen_name':account, 'count':num_tweets}
+        responses = caching.Check(url, params, auth)
+        tokens = []
+        for response in responses:
+            for word in nltk.word_tokenize(response['text']):
+                word = word.lower()
+                if word.isalpha() and word not in stop_words:
+                    tokens.append(word)
+        freq_list_by_account[account] = nltk.FreqDist(tokens)
+        print(freq_list_by_account[account].items())
+
+def most_common_shared_words(dicts, count=5):
+    common_words = {}
+    # order the dicts by the number of k:v pairs
+    ordered_dicts = sorted(dicts, key=lambda x: len(dicts[x]), reverse=True)
+    # iterate through the words in the first dictionary, since that will be the longest
+    for word in dicts[ordered_dicts[0]]:
+        if word in dicts[ordered_dicts[1]]:
+            common_words[word] = dicts[ordered_dicts[0]][word] + dicts[ordered_dicts[1]][word] #TODO: oh god this is ugly
+    common_words = sorted(common_words, key=lambda x: common_words[x], reverse=True)[:count]
+    return common_words
