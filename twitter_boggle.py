81365730016021421ad4f1be9a5b268a6c95272a
jonhartm <jonhartm@umich.edu>
2018-02-11 21:18:39 -0500

    moved most common words list to before the main code

--- twitter_boggle.py
+++ twitter_boggle.py
@@ -21,6 +21,17 @@ auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
 stop_words = stopwords.words('english') # from https://pythonspot.com/nltk-stop-words/
 stop_words.extend(['http', 'https', 'RT'])
 
+def most_common_shared_words(dicts, count=5):
+    common_words = {}
+    # order the dicts by the number of k:v pairs
+    ordered_dicts = sorted(dicts, key=lambda x: len(dicts[x]), reverse=True)
+    # iterate through the words in the first dictionary, since that will be the longest
+    for word in dicts[ordered_dicts[0]]:
+        if word in dicts[ordered_dicts[1]]:
+            common_words[word] = dicts[ordered_dicts[0]][word] + dicts[ordered_dicts[1]][word] #TODO: oh god this is ugly
+    common_words = sorted(common_words, key=lambda x: common_words[x], reverse=True)[:count]
+    return common_words
+
 if __name__ == '__main__':
     freq_list_by_account = {}
     for account in twitter_accounts:
@@ -34,15 +45,4 @@ if __name__ == '__main__':
                 if word.isalpha() and word not in stop_words:
                     tokens.append(word)
         freq_list_by_account[account] = nltk.FreqDist(tokens)
-        print(freq_list_by_account[account].items())
-
-def most_common_shared_words(dicts, count=5):
-    common_words = {}
-    # order the dicts by the number of k:v pairs
-    ordered_dicts = sorted(dicts, key=lambda x: len(dicts[x]), reverse=True)
-    # iterate through the words in the first dictionary, since that will be the longest
-    for word in dicts[ordered_dicts[0]]:
-        if word in dicts[ordered_dicts[1]]:
-            common_words[word] = dicts[ordered_dicts[0]][word] + dicts[ordered_dicts[1]][word] #TODO: oh god this is ugly
-    common_words = sorted(common_words, key=lambda x: common_words[x], reverse=True)[:count]
-    return common_words
+    print(most_common_shared_words(freq_list_by_account))
