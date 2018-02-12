7edbabe12e6a1cc21dd934f203c53a8decf4c4a3
jonhartm <jonhartm@umich.edu>
2018-02-11 21:55:08 -0500

    added comments

--- twitter_boggle.py
+++ twitter_boggle.py
@@ -21,6 +21,9 @@ auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
 stop_words = stopwords.words('english') # from https://pythonspot.com/nltk-stop-words/
 stop_words.extend(['http', 'https', 'rt'])
 
+# Gets the most common words that appear between two dictionaries
+# params: a dict formatted {'twitterUser1':{'token':X, ...}, 'twitterUser2':{'token':X, ...}} and a count of the number of words to return
+# returns: a list of strings
 def most_common_shared_words(dicts, count=5):
     common_words = {}
     # order the dicts by the number of k:v pairs
@@ -29,13 +32,17 @@ def most_common_shared_words(dicts, count=5):
     for word in dicts[ordered_dicts[0]]:
         if word in dicts[ordered_dicts[1]]:
             common_words[word] = dicts[ordered_dicts[0]][word] + dicts[ordered_dicts[1]][word] #TODO: oh god this is ugly
-    common_words = sorted(common_words, key=lambda x: common_words[x], reverse=True)[:count]
-    return common_words
+    return sorted(common_words, key=lambda x: common_words[x], reverse=True)[:count] # sort the words by total count between accounts and slice to the count
 
+# finds the most common words that do not appear in the other dictionary
+# params: a dict formatted {'twitterUser1':{'token':X, ...}, 'twitterUser2':{'token':X, ...}} and a count of the number of words to return
+# returns: a dictionary formatted {'twitterUser1':['token'], 'twitterUser1':['token']}
 def most_common_different_words(dicts, count=5):
+    # get every word that's common between the two accounts
     common_words = most_common_shared_words(dicts, -1)
     different_words = {}
     for d in dicts:
+        # find the most common words in this dictionary that don't appear in the common_words list
         different_words[d] = most_common_words_not_in(dicts[d], common_words, count)
     return different_words
 
@@ -43,9 +50,8 @@ def most_common_words_not_in(d, ignore_list, count):
     unique_words = {}
     for word in d:
         if word not in ignore_list:
-            unique_words[word] = d[word]
-    unique_words = sorted(unique_words, key=lambda x: unique_words[x], reverse=True)[:count]
-    return unique_words
+            unique_words[word] = d[word] # if the word doesn't appear in the ignore_list, add it to this dictionary
+    return sorted(unique_words, key=lambda x: unique_words[x], reverse=True)[:count] # sort the words by total count between accounts and slice to the count
 
 if __name__ == '__main__':
     freq_list_by_account = {}
